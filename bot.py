4bd2f1020cb4f64c9ae2e4ac53ff958e314b1bd0
handee <hmd@hannahdee.eu>
2018-09-10 12:38:56 +0100

    uploaded advicebot files

---
+++ bot.py
@@ -0,0 +1,55 @@
+import tweepy
+import random
+from secrets import *
+from time import sleep
+
+botname="@csfresheradvice"
+
+
+# advice bot, derived heavily from this 
+# "how to write a bot in python" tutorial
+# https://scotch.io/tutorials/build-a-tweet-bot-with-python
+# and this one
+# https://vivshaw.github.io/blog/build-you-a-tweetbot-1/
+
+
+class advicebot:
+    def __init__(self, advice ):
+        self.advice_lines=self.load_tweets(advice)
+
+        #initialize Twitter authorization with Tweepy
+        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+        auth.set_access_token(access_token, access_secret)
+        self.api = tweepy.API(auth)
+
+    def load_tweets(self, corpus):
+        with open(corpus) as corpus_file:
+            advice_lines = corpus_file.readlines()
+        return advice_lines
+    
+    def tweet(self):
+# randomly choose a message from your corpus
+        message = random.choice(self.advice_lines)
+# tweet it: note if you're trying stuff out coment the self.api.update line and uncomment the print line then you can see it working without spamming twitter 
+        try:
+            #print(message) 
+            self.api.update_status(message)
+        except tweepy.TweepError as error:
+            print(error.reason)
+        
+    def automate(self, delay):
+        while True:
+            self.tweet()
+            sleep(delay)
+
+
+def main():
+	bot = advicebot("advice.tsv")
+	bot.automate(3600)
+
+if __name__ == "__main__":
+    main()
+
+
+
+
---
+++ secrets.py
@@ -0,0 +1,5 @@
+# fill these with your actual twitter account api information
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_secret = ''
